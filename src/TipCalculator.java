a37a26607a742eca64fd3a9f397f7a22b8c9872e
BT_4N2_13 <yhuang6817@bths.edu>
2022-09-21 12:29:57 -0400

    I coded a tip calculator I added comments

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -4,22 +4,23 @@ public class TipCalculator {
         Scanner s = new Scanner(System.in);
 
         System.out.print("Total Bill: $");
-        Double totalBill = s.nextDouble();
+        Double totalBill = s.nextDouble(); //stores the user input in totalBill
 
         System.out.print("Tip Percentage: ");
-        int tipPercentage = s.nextInt();
+        int tipPercentage = s.nextInt(); //stores the user input in tipPercentage
 
         System.out.print("Number of People: ");
-        int numPeople = s.nextInt();
+        int numPeople = s.nextInt();  //stores the user input in numPeople
 
-        double tipAmount = ((double) tipPercentage / 100) * totalBill;
-        double totalPay = tipAmount + totalBill;
-        double tipPerPerson = tipAmount / numPeople;
-        double payPerPerson = totalPay / numPeople;
+        double tipAmount = ((double) tipPercentage / 100) * totalBill; //solves for the tip amount
+        double totalPay = tipAmount + totalBill;  //solves for the total amount of money paid
+        double tipPerPerson = tipAmount / numPeople; //solves for the amount of tip paid for person
+        double payPerPerson = totalPay / numPeople; //solves for total amount of money paid per person
 
         System.out.println("Tip Amount: $" + String.format("%.2f", tipAmount));
         System.out.println("Total Pay: $" + String.format("%.2f", totalPay));
         System.out.println("Tip per Person: $" + String.format("%.2f", tipPerPerson));
         System.out.println("Total Pay per Person: $" + String.format("%.2f", payPerPerson));
+        //prints out everything in the correct format
     }
 }
